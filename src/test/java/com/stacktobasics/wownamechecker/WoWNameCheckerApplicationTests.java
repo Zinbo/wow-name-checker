cac1e2e83c9887ef73ecfb7ef65d1c9e32977128
Zinbo <superzinbo@gmail.com>
2022-12-12 18:20:40 +0000

    Initialise project with Spring.

---
+++ src/test/java/com/stacktobasics/wownamechecker/WoWNameCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacktobasics.wownamechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoWNameCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
