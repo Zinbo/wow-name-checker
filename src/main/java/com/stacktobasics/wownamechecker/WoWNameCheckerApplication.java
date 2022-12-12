07306999af345552b3c0d49cb38c7e33dc5f4b76
Zinbo <superzinbo@gmail.com>
2022-12-12 23:28:56 +0000

    Initialise project with Spring.

---
+++ src/main/java/com/stacktobasics/wownamechecker/WoWNameCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.stacktobasics.wownamechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoWNameCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoWNameCheckerApplication.class, args);
+	}
+
+}
