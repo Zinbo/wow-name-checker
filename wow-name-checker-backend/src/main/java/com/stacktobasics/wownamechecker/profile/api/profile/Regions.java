cde465042862f0e9ca68d9aa87363fe320639fe5
Zinbo <superzinbo@gmail.com>
2023-05-10 23:19:32 +0100

    Use correct region names

--- wow-name-checker-backend/src/main/java/com/stacktobasics/wownamechecker/profile/api/profile/Regions.java
+++ wow-name-checker-backend/src/main/java/com/stacktobasics/wownamechecker/profile/api/profile/Regions.java
@@ -1,13 +1,16 @@
 package com.stacktobasics.wownamechecker.profile.api.profile;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.Map;
 import java.util.Optional;
 
 public class Regions {
-    private static final Map<String, String> value = Map.of("Europe", "eu", "Americas", "us");
+    private static final Map<String, String> value = Map.of("europe", "eu", "americas", "us");
 
-    public static Optional<String> getIdFromName(String name) {
-        return Optional.ofNullable(value.get(name));
+    public static Optional<String> getIdFromName(@NotNull String name) {
+        return Optional.ofNullable(value.get(name.toLowerCase()))
+                .or(() -> value.values().stream().filter(v -> v.equalsIgnoreCase(name)).findFirst());
     }
 
 }
