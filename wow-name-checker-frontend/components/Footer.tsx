042f76578e82e7db5a1105320c16728ec886c420
Zinbo <superzinbo@gmail.com>
2023-07-14 19:24:28 +0100

    Add privacy page and add to footer.

--- wow-name-checker-frontend/components/Footer.tsx
+++ wow-name-checker-frontend/components/Footer.tsx
@@ -11,10 +11,11 @@ export default function BottomAppBar() {
             <CssBaseline />
             <Box bgcolor='primary.main' sx={{ top: 'auto', bottom: 0, borderTop: '2px solid #F8B700' }}>
                 <Toolbar sx={{display: 'flex', flex: 1, justifyContent: 'space-between'}}>
+                    <Typography color={'text.secondary'} sx={{fontSize: '12px'}}>icons by <Link style={{color: '#FFF'}} target="_blank" href="https://icons8.com">Icons8</Link></Typography>
                     <Typography component='div' sx={{ mr: 1 }} color={'text.secondary'}>
                         Made with ❤️ in Nottingham by <Link target="_blank" href={"https://github.com/Zinbo"} style={{color: '#FFF'}}>Zinbo</Link>
                     </Typography>
-                    <Typography color={'text.secondary'} sx={{fontSize: '12px'}} >icons by <Link style={{color: '#FFF'}} target="_blank" href="https://icons8.com">Icons8</Link></Typography>
+                    <Typography color={'text.secondary'} sx={{fontSize: '12px'}}><Link style={{color: '#FFF'}}  href="/privacy" target="_blank">Privacy Policy</Link></Typography>
                 </Toolbar>
             </Box>
         </React.Fragment>
